c2ab070457b101f79558142f591a509b3dc91ed3
Fahri MacBook <fahri-can@web.de>
2018-04-28 19:43:20 +0200

    start test app

--- BroApp/ViewController.swift
+++ BroApp/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let friends = [
+        ("Fahri Can", "Favoriten"),
+        ("Marco Zema", "Ottakring"),
+        ("Matthias Muck", "Gerasdorf")
+    ]
+    
+    let universities = [
+        ("FH Technikum", "20.Bezirk"),
+        ("WU Wien", "2.Bezirk")
+    ]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return "Bro's 4 Life"
+        }
+        return "Hochschulen"
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            return friends.count
+        }
+        return universities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        if indexPath.section == 0 {
+            
+            var (personName, personAddress) = friends[indexPath.row]
+            cell.textLabel?.text = personName
+        }
+        else {
+            
+            var (universityName, universityAddress) = universities[indexPath.row]
+            cell.textLabel?.text = universityName
+        }
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
